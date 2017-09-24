3aa24677be35760e032db421734ce7aadda5a40c
RouxRC <b.ooghe@gmail.com>
2017-09-24 22:52:12 +0200

    first version

---
+++ bin/serve_https.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+print("serving https://localhost:4443/")
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
+httpd.serve_forever()
